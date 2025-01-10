62050486c7354094ac72c135458274559fd6fc72
orcas-6 <irouah2@gmail.com>
2025-01-10 23:49:20 +0100

    app run automation 2025-01-10_23

---
+++ clientNav.bash
@@ -0,0 +1,5 @@
+# navigate to the client directory and start the client server.
+echo "Starting client navigation...";
+
+cd ./client;
+npm run dev;
---
+++ serverNav.bash
@@ -0,0 +1,5 @@
+# navigate to the server directory and start the server.
+echo "Starting server navigation...";
+
+cd ./server;
+npm run dev;
